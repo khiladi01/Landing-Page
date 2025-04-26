dd61612ad7ca2f0759f6e806f0548b11f7379003
raushan778 <allmusiccenter404@gmail.com>
2025-04-26 23:51:40 +0530

    Updates

--- gym-landing-page/gym/src/App.jsx
+++ gym-landing-page/gym/src/App.jsx
@@ -1,4 +1,6 @@
 import bgimage from './assets/bgimage.jpg'
+import sideimage from './assets/sideimage.svg'
+import googleimage from './assets/googleimage.png'
 
 function App() {
   
@@ -6,25 +8,21 @@ function App() {
   return (
     <>
       <div className="h-auto w-full bg-slate-600 select-none ">
-      <div className="h-[100px] w-full bg-slate-300 shadow-md shadow-slate-950 sticky top-0 ">
-       <div className="p-[30px] ">
+      <div className="h-[60px] w-full bg-slate-400 shadow-md shadow-black sticky top-0 ">
+       <div className="p-[15px] ">
 
-        <span className="text-black font-semibold text-lg ">Gym Center</span>
+        <span className="text-white font-semibold text-[22px] font-mono ">Gym Center</span>
 
-        <span className="float-right text-slate-500 font-medium text-md hover:underline hover:underline-offset-8 decoration-slate-600 hover:text-slate-600 ">
-        <a href="#">Membership</a>
+        <span className="float-right ">
+        <button className='w-[150px] h-[30px] text-white font-medium text-md hover:text-slate-100 cursor-pointer outline-0 flex justify-center items-center rounded-full bg-slate-700 '>
+          Login
+        </button>
         </span>
 
-        <span className="float-right pr-3 text-slate-500 font-medium text-md hover:underline hover:underline-offset-8 decoration-slate-600 hover:text-slate-600 ">
-        <a href="#">Contact</a>
-        </span>
-
-        <span className="float-right pr-3 text-slate-500 font-medium text-md hover:underline hover:underline-offset-8 decoration-slate-600 hover:text-slate-600 ">
-        <a href="#">About</a>
-        </span>
-
-        <span className="float-right pr-3 text-slate-500 font-medium text-md underline underline-offset-8 decoration-slate-600 hover:text-slate-600 ">
-        <a href="#">Home</a>
+        <span className="float-right pr-5 ">
+        <button className='text-white font-medium text-md hover:text-slate-100 cursor-pointer'>
+          Sign Up
+        </button>
         </span>
 
        </div>
@@ -32,9 +30,33 @@ function App() {
       </div>
       {/* end div for nav bar */}
 
-      <div className="h-[700px] bg-center bg-cover  " style={{ backgroundImage: `url(${bgimage})` }}>
+      <div className="h-screen bg-center bg-cover flex justify-center items-center gap-30 " style={{ backgroundImage: `url(${bgimage})` }}>
       
-      
+      <div className=' '>
+      <header className='text-slate-50 text-6xl font-mono '>
+        Look & Feel 
+        <br></br>
+        Your Best Ever
+      </header>
+      <p className='text-slate-300 pt-[50px] w-[500px] '>
+        Get ready to build PREETY MUSCLES and find out just how fun getting healthy and stronger can be
+      </p>
+
+      <div className='pt-[50px] '>
+      <button className='text-slate-50  h-[70px] w-[350px] bg-rose-500 hover:bg-rose-600 cursor-pointer rounded-full text-lg font-medium'>
+        START FREE TRAIL
+      </button>
+      </div>
+
+      <div className='pt-[50px] cursor-pointer pl-[5px] '>
+        <img src={googleimage} alt="iamge" className='h-[50px] w-[150px] ' />
+      </div>
+
+      </div>
+
+      <div>
+      <img src={sideimage} alt="image" className=' w-[500px] ' />
+      </div>
 
       </div>
       </div>
